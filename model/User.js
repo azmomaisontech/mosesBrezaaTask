af13e7df33547fe10a91ce21748a143e22f476d6
AZMOMAISON <mosesaizee@outlook.com>
2020-09-22 10:42:44 +0100

    Created the UserModel

---
+++ model/User.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please add a valid email"],
+    unique: true
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: [true, "Please add a password"],
+    select: false
+  },
+  username: {
+    type: String,
+    required: [true, "Please add a username"],
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: [true, "Please add a first name"]
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please add a last name"]
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"]
+  },
+  location: {
+    //GeoJSON Point
+    type: {
+      type: String,
+      enum: ["Point"]
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere"
+    }
+  },
+  typeOfUser: {
+    type: String,
+    enum: ["seller", "client"],
+    default: "client"
+  },
+  profession: {
+    type: String
+  }
+});
+
+//To hash a password before saving
+UserSchema.pre("save", async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//To return a JWT to the client after sign up/in
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+//To compare provided password with the hashed password in the DB on signin
+UserSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
