07aa908d2e855d6a653692d569e867fad2e2d7e2
AZMOMAISON <mosesaizee@outlook.com>
2020-09-23 12:56:52 +0100

    Added the documentation URL

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRouter = require("./routes/users");
 const reviewRouter = require("./routes/reviews");
 const errorHandler = require("./middleware/error");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //load al config var
 dotenv.config({ path: "./config/config.env" });
@@ -14,6 +20,24 @@ connectDB();
 
 //Body parser
 app.use(express.json());
+
+//Sanitize Data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Protect XSS
+app.use(xss());
+
+//Prevent HTTP params polution
+app.use(hpp());
+
+//Enable Cors for Public Access
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mount Routers
 app.use("/api/v1/users", userRouter);
